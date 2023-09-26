d5aedee4735966241cfd27ca95d986e41987ccbf
Junaid Syed <sjunaid626@gmail.com>
2023-09-26 12:48:20 +0530

    Update greetings_controller.rb

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,2 +1,6 @@
 class Api::GreetingsController < ApplicationController
+   def random
+    random_greeting = Message.order('RANDOM()').first
+    render json: { message: random_greeting.content }
+  end
 end
