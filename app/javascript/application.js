8eacbc7c63e525d8349eaf3e1d8382636ca93264
Junaid Syed <sjunaid626@gmail.com>
2023-09-26 12:48:23 +0530

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,13 +2,26 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import store from './redux/store';
+import Greeting from './components/Greetings.js'
 
 function App() {
-  return (<h1>Hello World!</h1>);
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 ReactDOM.render(
-  <App/>,
+    <App/>,
   document.getElementById('root'),
+  
 );
